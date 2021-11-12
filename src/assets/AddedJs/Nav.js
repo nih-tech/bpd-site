619bee6be2f29d28f4c52480cdac645672c8a4e5
Zaeem <zaeemhassan72@gmail.com>
2021-11-12 15:53:08 +0500

    changes

--- src/assets/AddedJs/Nav.js
+++ src/assets/AddedJs/Nav.js
@@ -1,35 +1,8 @@
-
-document.addEventListener("DOMContentLoaded", function(){
-  // make it as accordion for smaller screens
-  if (window.innerWidth > 992) {
-  
-    document.querySelectorAll('.navbar .nav-item').forEach(function(everyitem){
-  
-      everyitem.addEventListener('mouseover', function(e){
-  
-        let el_link = this.querySelector('a[data-bs-toggle]');
-  
-        if(el_link != null){
-          let nextEl = el_link.nextElementSibling;
-          el_link.classList.add('show');
-          nextEl.classList.add('show');
-        }
-  
-      });
-      everyitem.addEventListener('mouseleave', function(e){
-        let el_link = this.querySelector('a[data-bs-toggle]');
-  
-        if(el_link != null){
-          let nextEl = el_link.nextElementSibling;
-          el_link.classList.remove('show');
-          nextEl.classList.remove('show');
-        }
-  
-  
-      })
-    });
-  
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
   }
-  // end if innerWidth
-  }); 
-  // DOMContentLoaded  end
+}
